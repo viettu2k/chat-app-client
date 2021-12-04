3b74e3698144abaa4e29669c4e4cb5866b77d600
viettu2k <doanviettu2k@gmail.com>
2021-12-04 11:25:06 +0700

    updating join compoent

--- src/components/Join.jsx
+++ src/components/Join.jsx
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { userState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Join() {
+  const [name, setName] = userState("");
+  const [room, setRoom] = userState("");
+
   return (
-    <div>
-      <h2>join</h2>
+    <div className="joinOuterContainer">
+      <div className="joinInnerContainer">
+        <h1 className="heading">Join</h1>
+        <div>
+          <input
+            className="joinInput"
+            type="text"
+            placeholder=""
+            onChange={(event) => setName(event.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            className="joinInput mt-20"
+            type="text"
+            placeholder=""
+            onChange={(event) => setRoom(event.target.value)}
+          />
+        </div>
+
+        <Link
+          onClick={(event) => (!name || !room ? event.preventDefault() : null)}
+          to={`/chat?name=${name}&room=${room}`}
+        >
+          <button className="button mt-20" type="submit">
+            Sign in
+          </button>
+        </Link>
+      </div>
     </div>
   );
 }
